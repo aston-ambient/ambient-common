f7caeec7a551dafb590b21297a3754c246ad8d7a
vNemesis <uppalhrs@aston.ac.uk>
2022-03-09 18:13:05 +0000

    added questionaire refereneces

---
+++ ambient-common/Models/QuestionaireReference.cs
@@ -0,0 +1,51 @@
+﻿using ambient_common.Questionaire;
+using System.Collections.Generic;
+using static ambient_common.Questionaire.QuestionaireEnums;
+
+namespace ambient_common.Models
+{
+    public class QuestionaireReference
+    {
+        public readonly QuestionCategories QuestionCategory;
+        public readonly QuestionTypes QuestionType;
+        public readonly string QuestionTitle;
+        public readonly string QuestionPlaceholderText = "";
+        public readonly List<string> SelectionOptions = new List<string>();
+        public readonly string SelectionAltText = "";
+        public readonly List<RankingQuestionItem> RankingOptions = new List<RankingQuestionItem>();
+
+        public QuestionaireReference(QuestionCategories questionCategory, QuestionTypes questionType, string questionTitle, string questionHint, List<string> selectionOptions, string selectionAltText)
+        {
+            QuestionCategory = questionCategory;
+            QuestionType = questionType;
+            QuestionTitle = questionTitle;
+            QuestionPlaceholderText = questionHint;
+            SelectionOptions = selectionOptions;
+            SelectionAltText = selectionAltText;
+        }
+
+        public QuestionaireReference(QuestionCategories questionCategory, QuestionTypes questionType, string questionTitle, string questionHint, List<RankingQuestionItem> rankingOptions)
+        {
+            QuestionCategory = questionCategory;
+            QuestionType = questionType;
+            QuestionTitle = questionTitle;
+            QuestionPlaceholderText = questionHint;
+            RankingOptions = rankingOptions;
+        }
+
+        public QuestionaireReference(QuestionCategories questionCategory, QuestionTypes questionType, string questionTitle, string questionHint)
+        {
+            QuestionCategory = questionCategory;
+            QuestionType = questionType;
+            QuestionTitle = questionTitle;
+            QuestionPlaceholderText = questionHint;
+        }
+
+        public QuestionaireReference(QuestionCategories questionCategory, QuestionTypes questionType, string questionTitle)
+        {
+            QuestionCategory = questionCategory;
+            QuestionType = questionType;
+            QuestionTitle = questionTitle;
+        }
+    }
+}
---
+++ ambient-common/Questionaire/QuestionaireEnums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ambient_common.Questionaire
+{
+    public class QuestionaireEnums
+    {
+        public enum QuestionCategories { General, ChildFusiness, ChildEmotion }
+        public enum QuestionTypes { Text, Number, Selection, SelectionAlt, Ranking }
+    }
+}
