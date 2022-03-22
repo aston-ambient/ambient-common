88f3589f6aafd2aedc3ff18837f8f3c0cf68254a
vNemesis <uppalhrs@aston.ac.uk>
2022-03-22 01:22:47 +0000

    fixed type mismatch for Ids

--- AmbientCommon/Models/ChildColourCustomisation.cs
+++ AmbientCommon/Models/ChildColourCustomisation.cs
@@ -5,11 +5,11 @@ namespace AmbientCommon.Models
     public class ChildColourCustomisation : BaseModel
     {
         public int Id { get; set; }
-        public int GameConfigurationId { get; set; }
         public string DisplayText { get; set; }
         public CustomisationCategory Category { get; set; }
         public string Value { get; set; }
-
+        public long GameConfigurationId { get; set; }
+        public GameConfiguration GameConfiguration { get; set; }
         public ChildColourCustomisation() {}
 
         public ChildColourCustomisation(string displayText, CustomisationCategory category, string value)
--- AmbientCommon/Models/Participant.cs
+++ AmbientCommon/Models/Participant.cs
@@ -2,7 +2,7 @@
 {
     public class Participant : BaseModel
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
         public string ParticipantReference { get; set; }
         public string GroupReference { get; set; }
     }
